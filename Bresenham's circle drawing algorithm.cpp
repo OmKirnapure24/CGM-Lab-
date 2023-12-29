c297031d911ee35cae537c8c75fbe6c4bb059140
OmKirnapure24 <102537324+OmKirnapure24@users.noreply.github.com>
2023-12-29 19:40:20 +0530

    Bresenham's circle drawing algorithm.cpp

---
+++ Bresenham's circle drawing algorithm.cpp
@@ -0,0 +1,38 @@
+#include <graphics.h>
+
+void drawCircle(int xc, int yc, int x, int y) {
+    putpixel(xc + x, yc + y, WHITE);
+    putpixel(xc - x, yc + y, WHITE);
+    putpixel(xc + x, yc - y, WHITE);
+    putpixel(xc - x, yc - y, WHITE);
+    putpixel(xc + y, yc + x, WHITE);
+    putpixel(xc - y, yc + x, WHITE);
+    putpixel(xc + y, yc - x, WHITE);
+    putpixel(xc - y, yc - x, WHITE);
+}
+
+void circleBres(int xc, int yc, int r) {
+    int x = 0, y = r;
+    int d = 3 - 2 * r;
+    drawCircle(xc, yc, x, y);
+    while (x <= y) {
+        x++;
+        if (d < 0) {
+            d = d + 4 * x + 6;
+        } else {
+            y--;
+            d = d + 4 * (x - y) + 10;
+        }
+        drawCircle(xc, yc, x, y);
+    }
+}
+
+int main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "");
+    int xc = 250, yc = 250, r = 100;
+    circleBres(xc, yc, r);
+    getch();
+    closegraph();
+    return 0;
+}
